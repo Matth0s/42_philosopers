0088a301a0a5e5d0b8b338427edf67df9316d2b0
Math0000 <matheus.moreira.jpa@gmail.com>
2021-12-16 14:28:16 -0300

    possivel conserto do tempo de impressão

--- philo/sources/start_simulation.c
+++ philo/sources/start_simulation.c
@@ -6,7 +6,7 @@
 /*   By: mmoreira <mmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 02:45:18 by mmoreira          #+#    #+#             */
-/*   Updated: 2021/12/16 12:33:24 by mmoreira         ###   ########.fr       */
+/*   Updated: 2021/12/16 14:23:57 by mmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,53 @@
 void	print_actions(t_philo *philo, char *action)
 {
 	long int	time;
-	int			philosoper;
+	int			p_num;
 
-	time = m_time() - philo->start;
-	philosoper = philo->p_num + 1;
 	pthread_mutex_lock(&philo->table->print);
 	if (!(philo->table->some_die))
 	{
+		time = m_time() - philo->table->start;
+		p_num = philo->p_num + 1;
 		if (*action == 'f' && *(action + 1) == 'l')
-			printf(PURPLE"%ld    %d has taken a fork\n"RESET, time, philosoper);
+			printf(PURPLE"%ld    %d has taken a fork\n"RESET, time, p_num);
 		else if (*action == 'f' && *(action + 1) == 'r')
-			printf(MAGENTA"%ld    %d has taken a fork\n"RESET, time, philosoper);
+			printf(MAGENTA"%ld    %d has taken a fork\n"RESET, time, p_num);
 		else if (*action == 'e')
-			printf(GREEN"%ld    %d is eating\n"RESET, time, philosoper);
+			printf(GREEN"%ld    %d is eating\n"RESET, time, p_num);
 		else if (*action == 's')
-			printf(CYAN"%ld    %d is sleeping\n"RESET, time, philosoper);
+			printf(CYAN"%ld    %d is sleeping\n"RESET, time, p_num);
 		else if (*action == 't')
-			printf(YELLOW"%ld    %d is thinking\n"RESET, time, philosoper);
+			printf(YELLOW"%ld    %d is thinking\n"RESET, time, p_num);
 		else
 		{
-			printf(RED"%ld    %d died\n"RESET, time, philosoper);
-			philo->table->some_die = philosoper;
+			printf(RED"%ld    %d died\n"RESET, time, p_num);
+			philo->table->some_die = p_num;
 		}
 	}
 	pthread_mutex_unlock(&philo->table->print);
 }
 
-void	philo_die(t_philo *philo)
+int		philo_die(t_philo *philo)
 {
-	if (philo->table->time_d < m_time() - philo->last_eat)
-		print_actions(philo, "die");
+	if (!philo->last_eat)
+	{
+		if (philo->table->time_d < m_time() - philo->table->start)
+		{
+			//printf("1  %d  -  %ld  -  %ld  -  %ld  -  %ld\n", philo->p_num + 1, philo->table->time_d, m_time() - philo->table->start, m_time(), philo->table->start );
+			print_actions(philo, "die");
+			return (1);
+		}
+	}
+	else
+	{
+		if (philo->table->time_d < m_time() - philo->last_eat)
+		{
+			//printf("2  %d  -  %ld  -  %ld  -  %ld -  %ld\n", philo->p_num + 1, philo->table->time_d,m_time() - philo->last_eat, m_time(), philo->last_eat );
+			print_actions(philo, "die");
+			return (1);
+		}
+	}
+	return (0);
 }
 
 void	*check_die(void *arg)
@@ -52,25 +69,30 @@ void	*check_die(void *arg)
 	t_philo *philo;
 
 	philo = (t_philo*)arg;
-	while (!(philo->table->some_die))
-		if (philo->last_eat)
-			philo_die(philo);
+	if (philo->p_num % 2 == 0)
+		m_sleep(1);
+	while (!(philo_die(philo)))
+		continue;
+	// while (philo && !(philo->table->some_die))
+	// 	continue;
+	// 	if (philo->last_eat)
+	// 		continue ;
+			//philo_die(philo);
 	return (NULL);
 }
 
 void	*simulation(void *arg)
 {
 	t_philo		*philo;
-	pthread_t	die;
+	//pthread_t	die;
 
 	philo = (t_philo *)arg;
-	while (!(philo->table->begin))
-		usleep(1);
+/* 	while (!(philo->table->begin))
+		usleep(1); */
 	if (philo->p_num % 2 == 1)
 		m_sleep(1);
-	pthread_create(&die, NULL, &check_die, philo);
-	pthread_detach(die);
-	philo->start = m_time();
+/* 	pthread_create(&die, NULL, &check_die, philo);
+	pthread_detach(die); */
 	while (!(philo->table->some_die))
 	{
 		philo_take_forks(philo);
@@ -91,10 +113,13 @@ int	start_simulation(t_table *table)
 
 	i = -1;
 	while (++i < table->n_phis)
+	{
+		table->start = m_time();
 		if (pthread_create(&(table->phis + i)->pth, NULL, &simulation,
 				table->phis + i))
 			return (1);
-	table->begin = 1;
+	}
+/* 	table->begin = 1; */
 	i = -1;
 	while (++i < table->n_phis)
 		if (pthread_join((table->phis + i)->pth, NULL))
